346d2b11c1e882390b31818b171f9607264c2fac
Anurag K V <anuragkv2002@gmail.com>
2024-09-08 19:46:29 +0530

    Add files via upload

---
+++ Anurag_Assigment/pro_app/src/module/Navbar.jsx
@@ -0,0 +1,23 @@
+import { AppBar, Button,  Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <AppBar>
+            <Toolbar>
+                <Typography variant='h3'component="div"  align="center" sx={{flexGrow:1}} >
+                    PRODUCT APP
+                </Typography>
+                <Link to="/add">
+                <Button variant="contained" >Add Product</Button></Link>&nbsp;&nbsp;
+                <Link to="/home">
+                <Button variant="contained" color="secondary">HOME</Button></Link>&nbsp;&nbsp;
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
